1dd3c4f774ffc0f7d2ce33920edc53de8f9ffd32
Erik Waterham <waterham@gmail.com>
2019-10-30 14:19:24 +0100

    first commit

---
+++ challenge-01/UnitConversion/UnitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Erik Waterham on 30/10/2019.
+//  Copyright © 2019 Erik Waterham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
